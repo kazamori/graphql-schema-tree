52df72d9b4b3bf60a7650701cdb86a7d27353f0d
Tetsuya Morimoto <tetsuya.morimoto@gmail.com>
2021-06-15 10:52:02 +0900

    add TraverseOption as default export

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 export { GraphQLSchemaTree } from "./graphqlSchemaTree";
-export { SchemaNodeHandler, Traversing } from "./handler";
+export { SchemaNodeHandler, TraverseOption, Traversing } from "./handler";
 export { ArgumentInfo, SchemaNode, SchemaNodeInfo, TypeInfo } from "./node";
 export { SchemaTree, SchemaTreeOption } from "./tree";

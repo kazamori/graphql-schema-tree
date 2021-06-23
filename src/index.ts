5de16eacbf8151acb5e8fec761142a6b4e8c575c
Tetsuya Morimoto <tetsuya.morimoto@gmail.com>
2021-06-23 20:43:22 +0900

    export types from handler

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,10 @@
 export { GraphQLSchemaTree } from "./graphqlSchemaTree";
-export { SchemaNodeHandler, TraverseOption, Traversing } from "./handler";
+export {
+  ArgumentValueInfo,
+  ConvertArgumentValueOption,
+  SchemaNodeHandler,
+  TraverseOption,
+  Traversing,
+} from "./handler";
 export { ArgumentInfo, SchemaNode, SchemaNodeInfo, TypeInfo } from "./node";
 export { SchemaTree, SchemaTreeOption } from "./tree";
